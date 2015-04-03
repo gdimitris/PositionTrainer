4919daee972407b0686c77d0249620bc56c24333
gdimitris <dimitrios.yiannakos@gmail.com>
2015-04-03 15:20:55 +0300

    Adds robolectric

---
+++ app/src/androidTest/java/com/example/dimitris/chesspositiontrainer/BoardTests.java
@@ -0,0 +1,48 @@
+package com.example.dimitris.chesspositiontrainer;
+
+import junit.framework.TestCase;
+
+
+/**
+ * Created by dimitris on 3/28/15.
+ */
+public class BoardTests extends TestCase {
+
+    private Board board;
+    private Piece piece;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        board = new Board();
+        piece = new Piece();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testGetsCorrectSquares(){
+        Square e4square = board.getSquareAt("e4");
+        assertEquals("e4",e4square.name);
+
+        Square e8square = board.getSquareAt("e8");
+        assertEquals("e8",e8square.name);
+
+        Square d5square = board.getSquareAt("d5");
+        assertEquals("d5",d5square.name);
+
+        Square h1square = board.getSquareAt("h1");
+        assertEquals("h1",h1square.name);
+
+        Square b7square = board.getSquareAt("b7");
+        assertEquals("b7",b7square.name);
+    }
+
+    public void testGetsPieceAtE4(){
+        board.setPieceAtSquare(piece,"e4");
+        assertEquals(piece,board.getPieceAt("e4"));
+    }
+
+}
---
+++ app/src/main/java/com/example/dimitris/chesspositiontrainer/PGNReader.java
@@ -0,0 +1,53 @@
+package com.example.dimitris.pgnparser;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by dimitris on 3/26/15.
+ */
+public class PGNReader {
+
+    public void readFile(InputStream inputStream){
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+        Pattern info_pattern = Pattern.compile("\\[[^\\]]*\\]");
+        Pattern whitespace_pattern = Pattern.compile("^\\s*$");
+        Pattern move_number_pattern = Pattern.compile("^\\d{1,2}\\.");
+        ArrayList<String> movesList = new ArrayList<String>();
+
+        try {
+            while( (line = reader.readLine()) != null ){
+
+                Matcher info_matcher = info_pattern.matcher(line);
+                Matcher whitespace_matcher = whitespace_pattern.matcher(line);
+                if (info_matcher.find()){
+                    Log.e("PGN Reading","I got info line : " + line);
+                } else if (whitespace_matcher.find()){
+                    Log.e("PGN Reading","I got whitespace");
+                } else {
+                    StringTokenizer tokenizer = new StringTokenizer(line);
+                    while (tokenizer.hasMoreTokens()){
+                        String token = tokenizer.nextToken();
+                        Matcher move_number_matcher = move_number_pattern.matcher(token);
+                        if(!move_number_matcher.matches()){
+                            movesList.add(token);
+                        }
+                    }
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Log.e("PGN Reading","The whole list is: "+ movesList.toString());
+    }
+}
